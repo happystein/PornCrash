a47e2035270e7426b98c1305ae4bc43416764316
Roman Stein <42235177+happystein@users.noreply.github.com>
2018-08-09 12:25:57 +0300

    Update Constants.java
    Fix url

--- porncrash/src/main/java/com/romanstein/porncrash/Constants.java
+++ porncrash/src/main/java/com/romanstein/porncrash/Constants.java
@@ -4,5 +4,5 @@ package com.romanstein.porncrash;
  * Roman Stein 08.08.2018.
  */
 public class Constants {
-    public static final String PORNHUB_URL = "https://google.com";
+    public static final String PORNHUB_URL = "https://pornhub.com";
 }
